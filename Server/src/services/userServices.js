b426af0dd495351ade4a368e9198f588c72fa086
Nebiyou <cnebiyu@gmail.com>
2024-10-11 13:36:58 +0300

    Update userServices.js

--- Server/src/services/userServices.js
+++ Server/src/services/userServices.js
@@ -49,7 +49,7 @@ async function loginUser({email , password}) {
     }
 
     const token = jwt.sign({userid:user.userid,username:user.username},
-        process.env.JWT_SECRETE,
+        process.env.JWT_SECRET,
         {expiresIn : '1d'});
     
     const {username} = user
